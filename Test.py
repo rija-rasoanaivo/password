3858e171d0ef9176d6958ed7f3ed190638a03f66
rija-rasoanaivo <rija.rasoanaivo@laplateforme.io>
2023-12-13 15:23:00 +0100

    Màj

--- Test.py
+++ Test.py
@@ -15,10 +15,10 @@ def caractere_speciaux(mot_de_passe): # Vérifie si le mot de passe contient un 
             return True # Retourne True si le mot de passe contient un caractère spécial 
     return False # Retourne False si le mot de passe ne contient pas de caractère spécial
 
-def mdp(new_mdp=None): # Fonction qui permet de vérifier si le mot de passe respecte les conditions
+def mdp(new_mdp=None): # Fonction principale  
     global chemin
     
-    while True: 
+    while True: #On entre dans la boucle
         if new_mdp is None:
             new_mdp = input("Entrez votre mot de passe : ")
 
@@ -41,23 +41,28 @@ def mdp(new_mdp=None): # Fonction qui permet de vérifier si le mot de passe res
             print("Votre mot de passe doit contenir au moins un caractère spécial parmi !, @, #, $, %, ^, &, * ")
         
         else:
-            h = hashlib.sha256(new_mdp.encode()).hexdigest()
-            with open(chemin, "r") as fichier:
+            nom_mot_de_passe = input("Entrez un nom pour ce mot de passe : ")  # Demander le nom du mot de passe
+            
+            h = hashlib.sha256(new_mdp.encode()).hexdigest()  # Calculer le hachage du mot de passe
+
+            mot_de_passe_info = {"nom": nom_mot_de_passe, "mot_de_passe_hache": h}  # Stocker le nom et le hachage du mot de passe
+
+            with open(chemin, "r") as fichier: # Ouverture du fichier password.json en mode lecture
                 try:
-                    liste = json.load(fichier)
-                except json.decoder.JSONDecodeError:
+                    liste = json.load(fichier) # Charger le contenu du fichier password.json dans la variable liste
+                except json.decoder.JSONDecodeError: # json.decoder.JSONDecodeError est une exception qui est levée lorsque le décodage JSON échoue
                     liste = []
                            
-            liste.append(h)
+            liste.append(mot_de_passe_info)  # Ajouter le mot de passe haché avec son nom dans la liste
             with open(chemin, "w") as fichier:
-                json.dump(liste, fichier, indent=3)
+                json.dump(liste, fichier, indent=3) #indent=3 permet d'indenter le fichier json pour une meilleure lisibilité
             
             print("Votre mot de passe est valide")
             print("Cryptage du mot de passe...")
-            print("Mot de passe crypté enregistré dans le fichier password.json")
-            afficher_mdp() # Appel de la fonction afficher_mdp
-            mdp_aleatoire() # Appel de la fonction mdp_aleatoire
-            ajouter_autre_mdp() # Appel de la fonction ajouter_autre_mdp
+            print("Mot de passe haché enregistré dans le fichier password.json avec le nom associé")
+            afficher_mdp()
+            mdp_aleatoire()
+            ajouter_autre_mdp() 
               
             return True
         new_mdp = input("Ecrire un mot de passe valide : ")
@@ -88,14 +93,20 @@ def mdp_aleatoire(): # Fonction qui permet de générer un mot de passe aléatoi
         for i in range(15):
             mot_de_passe += random.choice(majuscule + minuscule + chiffre + caractere_special)
         print("Voici votre mot de passe aléatoire : ", mot_de_passe)
+        
+        nom_mot_de_passe = input("Entrez un nom pour ce mot de passe : ")
+        
         h = hashlib.sha256(mot_de_passe.encode()).hexdigest() # Cryptage du mot de passe avec la fonction sha256 de la librairie hashlib # Conversion du mot de passe en hexadecimal # Stockage du mot de passe crypté dans la variable h
+        
+        mot_de_passe_info = {"nom": nom_mot_de_passe, "mot_de_passe_hache": h}
+        
         with open(chemin, "r") as fichier:
             try:
                 liste = json.load(fichier)
             except json.decoder.JSONDecodeError:
                 liste = []
                         
-        liste.append(h) # Ajout du mot de passe crypté dans la liste
+        liste.append(mot_de_passe_info) # Ajout du mot de passe crypté dans la liste
         with open(chemin, "w") as fichier: # Ouverture du fichier password.json en mode ajout # Si le fichier n'existe pas, il sera créé automatiquement
             json.dump(liste, fichier, indent=3) # Ecriture du mot de passe crypté dans le fichier password.json
             
