6745994b415f47397eb5bddb06e2b69285ca20b1
rija-rasoanaivo <rija.rasoanaivo@laplateforme.io>
2023-12-13 14:53:26 +0100

    Màj

--- Test.py
+++ Test.py
@@ -1,25 +1,31 @@
+#import secrets
 import json
 import hashlib
+import random
 
-chemin = r'C:\Users\rijar\Desktop\Ecole_Laplateforme\Projet\password\password\password.json'
+#nombres_caracteres = 8
 
-def caractere_speciaux(mot_de_passe):
-    caractere_special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+']
-    for index in caractere_special:
-        if index in mot_de_passe:
-            return True
-    return False
+caractere_special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+']
+chemin = 'password.json'
 
-def saisir_mot_de_passe():
-    passwords = []  # Liste pour stocker les mots de passe cryptés
+def caractere_speciaux(mot_de_passe): # Vérifie si le mot de passe contient un caractère spécial
+    global caractere_special # Permet d'utiliser la variable caractere_special qui est en dehors de la fonction
+    for index in caractere_special: # Pour chaque caractère spécial dans la liste caractere_special 
+        if index in mot_de_passe: # Si le caractère spécial est dans le mot de passe
+            return True # Retourne True si le mot de passe contient un caractère spécial 
+    return False # Retourne False si le mot de passe ne contient pas de caractère spécial
+
+def mdp(new_mdp=None): # Fonction qui permet de vérifier si le mot de passe respecte les conditions
+    global chemin
     
-    while True:
-        new_mdp = input("Entrez un mot de passe : ")
-        
+    while True: 
+        if new_mdp is None:
+            new_mdp = input("Entrez votre mot de passe : ")
+
         if len(new_mdp) < 8:
             print("Votre mot de passe doit contenir au moins huit caractères")
-        # Autres conditions pour vérifier les majuscules, minuscules, chiffres et caractères spéciaux
-        elif new_mdp.islower(): # Si le mot de passe contient que des lettres minuscules # islower() est une fonction qui permet de vérifier si le mot de passe contient que des lettres minuscules
+        
+        elif new_mdp.islower():
             print("Votre mot de passe contient que des lettres minuscules, il ne respecte pas les conditions")
         
         elif new_mdp.isupper():
@@ -31,30 +37,73 @@ def saisir_mot_de_passe():
         elif new_mdp.isalpha():
             print("Votre mot de passe contient que des lettres, il ne respecte pas les conditions")
         
-        elif caractere_speciaux(new_mdp) == False: # Si la fonction caractere_speciaux retourne False
-            print("Votre mot de passe doit contenir au moins un caractère spécial !, @, #, $, %, ^, &, * ")
+        elif not caractere_speciaux(new_mdp):
+            print("Votre mot de passe doit contenir au moins un caractère spécial parmi !, @, #, $, %, ^, &, * ")
+        
         else:
-            if caractere_speciaux(new_mdp) == False:
-                print("Votre mot de passe doit contenir au moins un caractère spécial")
-            else:
-                h = hashlib.sha256(new_mdp.encode()).hexdigest()
-                passwords.append(h)  # Ajouter le hachage à la liste des mots de passe
-                
-                print(f"Votre mot de passe '{new_mdp}' est valide")
-                print("Cryptage du mot de passe...")
-                print(f"Mot de passe crypté: \n{h}")
-                
-                sauvegarder_mots_de_passe(passwords)  # Enregistrer la liste mise à jour dans le fichier JSON
-                break
+            h = hashlib.sha256(new_mdp.encode()).hexdigest()
+            with open(chemin, "r") as fichier:
+                try:
+                    liste = json.load(fichier)
+                except json.decoder.JSONDecodeError:
+                    liste = []
+                           
+            liste.append(h)
+            with open(chemin, "w") as fichier:
+                json.dump(liste, fichier, indent=3)
+            
+            print("Votre mot de passe est valide")
+            print("Cryptage du mot de passe...")
+            print("Mot de passe crypté enregistré dans le fichier password.json")
+            afficher_mdp() # Appel de la fonction afficher_mdp
+            mdp_aleatoire() # Appel de la fonction mdp_aleatoire
+            ajouter_autre_mdp() # Appel de la fonction ajouter_autre_mdp
+              
+            return True
+        new_mdp = input("Ecrire un mot de passe valide : ")
+             
+def afficher_mdp(): # Fonction qui permet d'afficher le mot de passe crypté
+    reponse = input("Voulez-vous afficher le mot de passe crypté ? (Oui/Non) : ")
+    if reponse == "Oui" or reponse == "oui" or reponse == "OUI" or reponse == "O" or reponse == "o":
+        with open(chemin, "r") as fichier:
+            print(fichier.read())
+    else:
+        print("Mot de passe crypté non affiché")
 
-def sauvegarder_mots_de_passe(passwords):
-    with open(chemin, "w") as fichier:
-        json.dump(passwords, fichier)
-        print("Mots de passe cryptés enregistrés dans le fichier password.json")
+def ajouter_autre_mdp(): # Fonction qui permet d'ajouter un autre mot de passe
+    reponse = input("Voulez-vous ajouter un autre mot de passe ? (Oui/Non) : ")
+    if reponse == "Oui" or reponse == "oui" or reponse == "OUI" or reponse == "O" or reponse == "o":
+        mdp()
+    else:
+        print("D'accord")
 
-def afficher_mots_de_passe():
-    with open(chemin, "r") as fichier:
-        print(f"Mots de passe cryptés : {json.load(fichier)}")
+def mdp_aleatoire(): # Fonction qui permet de générer un mot de passe aléatoire
+    majuscule = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    minuscule = "abcdefghijklmnopqrstuvwxyz"
+    chiffre = "0123456789"
+    caractere_special = "!@#$%^&*()_+"
+    mot_de_passe = ""
+    reponse = input("Voulez-vous générer un mot de passe aléatoire ? (Oui/Non) : ")
+    if reponse == "Oui" or reponse == "oui" or reponse == "OUI" or reponse == "O" or reponse == "o":
+        for i in range(15):
+            mot_de_passe += random.choice(majuscule + minuscule + chiffre + caractere_special)
+        print("Voici votre mot de passe aléatoire : ", mot_de_passe)
+        h = hashlib.sha256(mot_de_passe.encode()).hexdigest() # Cryptage du mot de passe avec la fonction sha256 de la librairie hashlib # Conversion du mot de passe en hexadecimal # Stockage du mot de passe crypté dans la variable h
+        with open(chemin, "r") as fichier:
+            try:
+                liste = json.load(fichier)
+            except json.decoder.JSONDecodeError:
+                liste = []
+                        
+        liste.append(h) # Ajout du mot de passe crypté dans la liste
+        with open(chemin, "w") as fichier: # Ouverture du fichier password.json en mode ajout # Si le fichier n'existe pas, il sera créé automatiquement
+            json.dump(liste, fichier, indent=3) # Ecriture du mot de passe crypté dans le fichier password.json
+            
+        return mot_de_passe
+    else:
+        print("D'accord")
+        return reponse
 
-saisir_mot_de_passe()
-afficher_mots_de_passe()
+mot_de_passe = input("Votre mot de passe doit contenir :\n- au moins huit caractères\n- au moins une majuscule\n- au moins une minuscule\n- au moins un chiffre\n- au moins un caractère spécial (!, @, #, $, %, ^, &, *)\nEntrez un mot de passe : ")
+
+mdp(mot_de_passe)
--- password.py
+++ password.py
@@ -1,12 +1,10 @@
-#import random
 #import secrets
 import json
 import hashlib
+import random
 
 #nombres_caracteres = 8
-#majuscule = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-#minuscule = "abcdefghijklmnopqrstuvwxyz"
-#chiffre = "0123456789"
+
 caractere_special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+']
 chemin = 'password.json'
 
@@ -17,13 +15,17 @@ def caractere_speciaux(mot_de_passe): # Vérifie si le mot de passe contient un 
             return True # Retourne True si le mot de passe contient un caractère spécial 
     return False # Retourne False si le mot de passe ne contient pas de caractère spécial
 
-def mdp(new_mdp):
-    while True:
-        
+def mdp(new_mdp=None): # Fonction qui permet de vérifier si le mot de passe respecte les conditions
+    global chemin
+    
+    while True: 
+        if new_mdp is None:
+            new_mdp = input("Entrez votre mot de passe : ")
+
         if len(new_mdp) < 8:
             print("Votre mot de passe doit contenir au moins huit caractères")
         
-        elif new_mdp.islower(): # Si le mot de passe contient que des lettres minuscules # islower() est une fonction qui permet de vérifier si le mot de passe contient que des lettres minuscules
+        elif new_mdp.islower():
             print("Votre mot de passe contient que des lettres minuscules, il ne respecte pas les conditions")
         
         elif new_mdp.isupper():
@@ -35,43 +37,73 @@ def mdp(new_mdp):
         elif new_mdp.isalpha():
             print("Votre mot de passe contient que des lettres, il ne respecte pas les conditions")
         
-        elif caractere_speciaux(new_mdp) == False: # Si la fonction caractere_speciaux retourne False
-            print("Votre mot de passe doit contenir au moins un caractère spécial !, @, #, $, %, ^, &, * ")
+        elif not caractere_speciaux(new_mdp):
+            print("Votre mot de passe doit contenir au moins un caractère spécial parmi !, @, #, $, %, ^, &, * ")
         
         else:
-            h = hashlib.sha256(mot_de_passe.encode()).hexdigest()# Cryptage du mot de passe avec la fonction sha256 de la librairie hashlib # Conversion du mot de passe en hexadecimal # Stockage du mot de passe crypté dans la variable h
+            h = hashlib.sha256(new_mdp.encode()).hexdigest()
             with open(chemin, "r") as fichier:
                 try:
                     liste = json.load(fichier)
                 except json.decoder.JSONDecodeError:
                     liste = []
                            
-            liste.append(h) # Ajout du mot de passe crypté dans la liste
-            with open(chemin, "w") as fichier: # Ouverture du fichier password.json en mode ajout # Si le fichier n'existe pas, il sera créé automatiquement 
-                json.dump(liste, fichier, indent=3) # Ecriture du mot de passe crypté dans le fichier password.json 
-            print(f"Votre mot de passe '{new_mdp}' est valide")
+            liste.append(h)
+            with open(chemin, "w") as fichier:
+                json.dump(liste, fichier, indent=3)
+            
+            print("Votre mot de passe est valide")
             print("Cryptage du mot de passe...")
-            print(f"Mot de passe crypté: \n{h}")
             print("Mot de passe crypté enregistré dans le fichier password.json")
-
+            afficher_mdp() # Appel de la fonction afficher_mdp
+            mdp_aleatoire() # Appel de la fonction mdp_aleatoire
+            ajouter_autre_mdp() # Appel de la fonction ajouter_autre_mdp
+              
             return True
-        new_mdp = input("écrire un mot de passe valide : ")
-
-mot_de_passe = input("Votre mot de passe doit contenir: \n-au moins huit caractères \n-au moins une majuscule \n-au moins une minuscule \n-au moins un chiffre \n-au moins un caractère spécial(!, @, #, $, %, ^, &, *) \nEntrez un mot de passe : ")
-
-mdp(mot_de_passe)  
-
+        new_mdp = input("Ecrire un mot de passe valide : ")
+             
 def afficher_mdp(): # Fonction qui permet d'afficher le mot de passe crypté
-        input("Voulez-vous afficher le mot de passe crypté ? (Oui/Non) : ")
-        if input == "Oui" or "oui":
-            with open(chemin, "r") as fichier: # Ouverture du fichier password.json en mode lecture
-                print(fichier.read()) # Affiche le contenu du fichier password.json
-        elif input == "Non" or "non":
-            print("Mot de passe crypté non affiché")
+    reponse = input("Voulez-vous afficher le mot de passe crypté ? (Oui/Non) : ")
+    if reponse == "Oui" or reponse == "oui" or reponse == "OUI" or reponse == "O" or reponse == "o":
+        with open(chemin, "r") as fichier:
+            print(fichier.read())
+    else:
+        print("Mot de passe crypté non affiché")
 
-def supprimer_mdp(): # Fonction qui permet de supprimer le mot de passe crypté
-    with open(chemin, "w") as fichier: # Ouverture du fichier password.json en mode écriture
-        fichier.write("") # Ecriture d'un espace vide dans le fichier password.json
-    print("Mot de passe crypté supprimé")
+def ajouter_autre_mdp(): # Fonction qui permet d'ajouter un autre mot de passe
+    reponse = input("Voulez-vous ajouter un autre mot de passe ? (Oui/Non) : ")
+    if reponse == "Oui" or reponse == "oui" or reponse == "OUI" or reponse == "O" or reponse == "o":
+        mdp()
+    else:
+        print("D'accord")
 
-afficher_mdp()
+def mdp_aleatoire(): # Fonction qui permet de générer un mot de passe aléatoire
+    majuscule = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    minuscule = "abcdefghijklmnopqrstuvwxyz"
+    chiffre = "0123456789"
+    caractere_special = "!@#$%^&*()_+"
+    mot_de_passe = ""
+    reponse = input("Voulez-vous générer un mot de passe aléatoire ? (Oui/Non) : ")
+    if reponse == "Oui" or reponse == "oui" or reponse == "OUI" or reponse == "O" or reponse == "o":
+        for i in range(15):
+            mot_de_passe += random.choice(majuscule + minuscule + chiffre + caractere_special)
+        print("Voici votre mot de passe aléatoire : ", mot_de_passe)
+        h = hashlib.sha256(mot_de_passe.encode()).hexdigest() # Cryptage du mot de passe avec la fonction sha256 de la librairie hashlib # Conversion du mot de passe en hexadecimal # Stockage du mot de passe crypté dans la variable h
+        with open(chemin, "r") as fichier:
+            try:
+                liste = json.load(fichier)
+            except json.decoder.JSONDecodeError:
+                liste = []
+                        
+        liste.append(h) # Ajout du mot de passe crypté dans la liste
+        with open(chemin, "w") as fichier: # Ouverture du fichier password.json en mode ajout # Si le fichier n'existe pas, il sera créé automatiquement
+            json.dump(liste, fichier, indent=3) # Ecriture du mot de passe crypté dans le fichier password.json
+            
+        return mot_de_passe
+    else:
+        print("D'accord")
+        return reponse
+
+mot_de_passe = input("Votre mot de passe doit contenir :\n- au moins huit caractères\n- au moins une majuscule\n- au moins une minuscule\n- au moins un chiffre\n- au moins un caractère spécial (!, @, #, $, %, ^, &, *)\nEntrez un mot de passe : ")
+
+mdp(mot_de_passe)
